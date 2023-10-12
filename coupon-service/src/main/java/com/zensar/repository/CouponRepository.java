aae18f1d31e3649a76a49a9bfccc4712a030e613
priyaghadge99 <priyaghadge59@gmail.com>
2023-10-12 16:39:09 +0530

    product Coupon Service added with -openfein logic/Communication between microservice

---
+++ coupon-service/src/main/java/com/zensar/repository/CouponRepository.java
@@ -0,0 +1,12 @@
+package com.zensar.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.zensar.entity.Coupon;
+
+@Repository
+public interface CouponRepository extends JpaRepository<Coupon, Integer> {
+	
+		Coupon getByCouponCode(String couponCode);
+}
